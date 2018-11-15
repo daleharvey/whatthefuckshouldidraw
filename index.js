e3c6a420eeb96df56f5d5c3e9c144e2c8b8300a2
Dale Harvey <dale@arandomurl.com>
2018-11-15 10:57:08 +0000

    Add all prompts and update css for mobile

--- index.js
+++ index.js
@@ -25,19 +25,57 @@ let drawings = shuffle.shuffle([
   'Some fucking antlers!',
   'A pair of fucking wings!',
   'A fucking skull!',
-  'Some fucking eyes',
-  'A dark fucking cave',
-  'Some fancy fucking cocktails',
+  'Some fucking eyes!',
+  'A dark fucking cave!',
+  'Some fancy fucking cocktails!',
   'A fucking crab!',
-  'A fucking heart',
-  'A fucking unicorn',
-  'Some fucking trees'
+  'A fucking heart!',
+  'A fucking unicorn!',
+  'Some fucking trees!',
+  'A fucking tiger!',
+  'A fucking mountain!',
+  'A cute fucking fox!',
+  'A cute fucking penguin!',
+  'Some cool fucking hair!',
+  'A cute baby!',
+  'A pretty fucking lake!',
+  'A fucking globe!',
+  'A fucking space astronaut!',
+  'A long fucking pier',
+  'A nice fucking street',
+  'Your favourite fucking building',
+  'A sinister fucking character',
+  'A fucking fancy lady',
+  'A fucking Tuk Tuk',
+  'A pretty fucking fountain',
+  'A cosy fucking cup of tea',
+  'A fucking nice pair of kicks',
+  'A fucking snail',
+  'Some fucking bagpipes',
+  'A fucking fancy camel',
+  'A fucking boat',
+  'A fucking submarine',
+  'Some fucking spiders',
+  'A fucking skyline',
+  'A bottle of your favourite fucking drink',
+  'A fucking fire',
+  'A fucking castle',
+  'A big fucking statue',
+  'A fucking old dinosaur!',
+  'A big fucking tornado!',
+  'A fucking bridge',
+  'A cool fucking map',
+  'A fucking massive iceberg',
+  'A fucking scary zombie',
 ], 'whatthefuckshouldidraw');
 
 // But I already decided this is the first topic
 drawings.unshift('A fucking beautiful swan!');
 
-console.log(drawings.length);
+// And duh
+drawings.splice(6, 0, 'A fucking santa obviously');
+
+console.log(drawings.length)
 
 function weeksBetween(d1, d2) {
   return Math.round((d2 - d1) / (7 * 24 * 60 * 60 * 1000));
@@ -47,9 +85,9 @@ router.get('/', (ctx, next) => {
 
   let date = new Date();
   let currentWeek = new Date(date.setDate(date.getDate() - date.getDay()));
-  let weekStr = currentWeek.getFullYear() + '/' +
+  let weekStr =  currentWeek.getDate() + '/' +
       (currentWeek.getMonth() + 1) + '/' +
-      currentWeek.getDate();
+      currentWeek.getFullYear();
   let weeksSince = weeksBetween(start, currentWeek);
 
   ctx.body = index
